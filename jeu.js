74b4208fe8dca95a63d3c2052f1ca8d37d0a3bd8
CS-NSI <58935796+CS-NSI@users.noreply.github.com>
2020-01-27 11:59:32 +0100

    Add files via upload

---
+++ jeu.js
@@ -0,0 +1,43 @@
+function leftArrowPressed() {
+    var element = document.getElementById("image1");
+    element.style.left = parseInt(element.style.left) - 15 + 'px';
+    }
+
+    function rightArrowPressed() {
+    var element = document.getElementById("image1");
+    element.style.left = parseInt(element.style.left) + 15 + 'px';
+
+    }
+
+    function upArrowPressed() {
+    var element = document.getElementById("image1");
+    element.style.top = parseInt(element.style.top) - 15 + 'px';
+    }
+
+    function downArrowPressed() {
+    var element = document.getElementById("image1");
+    element.style.top = parseInt(element.style.top) + 15 + 'px';
+    }
+
+    function moveSelection(evt) {
+        switch (evt.keyCode) {
+            case 37:
+            leftArrowPressed();
+            break;
+            case 39:
+            rightArrowPressed();
+            break;
+            case 38:
+            upArrowPressed();
+            break;
+            case 40:
+            downArrowPressed();
+            break;
+            }
+        };
+
+function docReady()
+{
+  
+  window.addEventListener('keydown', moveSelection);
+}
